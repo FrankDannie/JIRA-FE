aa5d1556a368d4347b3cda6940c146efe01f3546
FrankDannie <frank.dannie@gmail.com>
2024-08-10 14:05:57 +0900

    Update footer Funtionality

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/footer.module.scss'
 
 const Footer: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    setIsLoggedIn(!!token)
+  }, [])
+
+  if (isLoggedIn) {
+    return null
+  }
+
   return (
     <footer className={styles.footer}>
       <p>&copy; 2024 Project Management Tool. All rights reserved.</p>
